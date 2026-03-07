835a3ef937019bcfdfbb296a4c56d50f0674f134
Harsh Raj <hr4798824@gmail.com>
2026-03-07 14:02:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ load_dotenv()
 DB_CONFIG = {
     "host": os.getenv("DB_HOST", "localhost"),
     "user": os.getenv("DB_USER", "root"),
-    "password": os.getenv("DB_PASSWORD", "harsh@618"),
-    "database": os.getenv("DB_NAME", "crop_yield"),
+    "password": os.getenv("DB_PASSWORD", "Your_db_password"),
+    "database": os.getenv("DB_NAME", "Your_db_name"),
     "port": int(os.getenv("DB_PORT", 3306))
 }
 
